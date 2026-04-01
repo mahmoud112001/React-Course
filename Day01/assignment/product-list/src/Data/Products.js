a639c6a968ac7d2bb1c944a8fb46162cf3ba472a
mahmoud112001 <mahmoudawad112001@gmail.com>
2026-04-01 23:43:10 +0200

    feat: add mock data for product listing

---
+++ Day01/assignment/product-list/src/Data/Products.js
@@ -0,0 +1,45 @@
+import product1 from "../assets/product1.jfif";
+import product2 from "../assets/download.jfif";
+import product3 from "../assets/bleu-de-chanel-eau-de-parfum-product-photography-ai-tool-after.jpg";
+import product4 from "../assets/Product+Photography+Course.webp";
+import product5 from "../assets/images.jfif";
+import product6 from "../assets/images(1).jfif";
+const products = [
+  {
+    id: 1,
+    name: "product-1",
+    price: 1200,
+    image: product1,
+  },
+  {
+    id: 2,
+    name: "product-2",
+    price: 1300,
+    image: product2,
+  },
+  {
+    id: 3,
+    name: "product-3",
+    price: 1450,
+    image: product3,
+  },
+  {
+    id: 4,
+    name: "product-4",
+    price: 2300,
+    image: product4,
+  },
+  {
+    id: 5,
+    name: "product-5",
+    price: 3400,
+    image: product5,
+  },
+  {
+    id: 6,
+    name: "product-6",
+    price: 1600,
+    image: product6,
+  },
+];
+export default products;
