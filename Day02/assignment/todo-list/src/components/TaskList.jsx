14459a6be52e774331c8fb5863a68a6df191e92c
mahmoud112001 <mahmoudawad112001@gmail.com>
2026-04-06 11:35:09 +0200

    Refactor: improve TaskList state management

--- Day02/assignment/todo-list/src/components/TaskList.jsx
+++ Day02/assignment/todo-list/src/components/TaskList.jsx
@@ -3,15 +3,15 @@ import TaskItem from "./TaskItem";
 export default function TaskList({ tasks, onToggle, onDelete }) {
   if (tasks.length === 0) {
     return (
-      <div className="text-center py-12 text-gray-400">
-        <i className="fa-regular fa-clipboard text-4xl mb-3 block"></i>
-        <p className="text-sm">There are no tasks to show </p>
+      <div className="text-center py-10 text-gray-400 text-sm">
+        <i className="fa-regular fa-clipboard text-3xl mb-2 block"></i>
+        No tasks here.
       </div>
     );
   }
 
   return (
-    <ul className="space-y-2">
+    <ul>
       {tasks.map((task) => (
         <TaskItem
           key={task.id}
