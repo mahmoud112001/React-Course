5a43aec4a158d23240822c4dafa42685628acafc
mahmoud112001 <mahmoudawad112001@gmail.com>
2026-04-07 11:22:28 +0200

    feat: initialize lab01 App component

---
+++ Day01/lab01/src/App.jsx
@@ -0,0 +1,24 @@
+import Button from "./components/Button";
+const App =() => {
+const Name="mahmoud Awad"
+
+  return (
+    <div className=" bg-amber-200 p-4 marker: *:not-odd:*">
+      <h1 className="bg-slate-700 font-bold text-2xl ">hello  {Name}</h1>
+    
+ <Button ppp="mahmoud" />
+ <Button ppp="aa" />
+ <Button ppp="aasx" />
+ <Button ppp="weqr" />
+ <Button  />
+ <Button  />
+    <h1 className="text-3xl font-bold underline text-blue-600">
+  Hello Tailwind!
+</h1>
+</div>
+
+  );
+}
+
+
+export default App;
