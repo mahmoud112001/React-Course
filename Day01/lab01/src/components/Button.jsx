a0b91cc6af7916f180854f3bd01d017f2310c163
mahmoud112001 <mahmoudawad112001@gmail.com>
2026-04-07 11:25:07 +0200

    feat: create reusable Button component

---
+++ Day01/lab01/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({ppp}) => {
+  return (
+    <div>
+      <button className='bg-yellow-900 border-2 border-black '  > hello + {ppp ? ppp : "base"} fuc</button>
+    </div>
+  )
+}
+
+export default Button
